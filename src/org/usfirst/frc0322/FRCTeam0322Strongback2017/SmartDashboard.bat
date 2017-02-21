56309d73bcae283c0085f4332435fb4e9debde77
Raa'Shaun H <CardcaptorRaaShaun@gmail.com>
2017-02-21 16:36:53 -0500

    Batch file to start the correct driver station.

---
+++ src/org/usfirst/frc0322/FRCTeam0322Strongback2017/SmartDashboard.bat
@@ -0,0 +1,5 @@
+rem This is set to run on my system switch the lines to use on another driver station.
+cd C:\Users\CCRLH85\wpilib\tools\
+rem cd C:\Users\Developer\wpilib\tools\
+rem cd %UserProfile%\wpilib\tools\
+java -jar SmartDashboard.jar Steamworks2017.xml
